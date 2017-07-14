551e7080c272a18b833a7a5b4791fd822cc2f607
ekim511 <ekim511@gmail.com>
2017-07-14 10:56:51 -0400

    Makestagram Step 20
    Added feature: followers

---
+++ Makestagram/FindFriendsCell.swift
@@ -0,0 +1,46 @@
+//
+//  FindFriendsCell.swift
+//  Makestagram
+//
+//  Created by Eliott Kim on 7/14/17.
+//  Copyright © 2017 Eliott Kim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol FindFriendsCellDelegate: class {
+    func didTapFollowButton(_ followButton: UIButton, on cell: FindFriendsCell)
+}
+
+class FindFriendsCell : UITableViewCell {
+    
+    // MARK: - Cell Lifecycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        followButton.layer.borderColor = UIColor.lightGray.cgColor
+        followButton.layer.borderWidth = 1
+        followButton.layer.cornerRadius = 6
+        followButton.clipsToBounds = true
+        
+        followButton.setTitle("Follow", for: .normal)
+        followButton.setTitle("Following", for: .selected)
+    }
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var followButton: UIButton!
+    
+    weak var delegate: FindFriendsCellDelegate?
+    
+    //MARK: - IBActions
+    
+    @IBAction func followButtonTapped(_ sender: UIButton) {
+        delegate?.didTapFollowButton(sender, on: self)
+    }
+
+}
--- Makestagram/UserService.swift
+++ Makestagram/UserService.swift
@@ -74,7 +74,57 @@ struct UserService {
                 completion(posts)
             })
         })
-    }    
+    }
+    
+    static func usersExcludingCurrentUser(completion: @escaping ([User]) -> Void) {
+        let currentUser = User.current
+        
+        // 1 Create a DatabaseReference to read all users from the database
+        let ref = Database.database().reference().child("users")
+        
+        // 2 Read the users node from the database
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let snapshot = snapshot.children.allObjects as? [DataSnapshot]
+                else { return completion([]) }
+            
+            // 3 Take the snapshot and perform a few transformations. First, we convert all of the child DataSnapshot into User using our failable initializer. Next we filter out the current user object from the User array
+            let users =
+                snapshot
+                    .flatMap(User.init)
+                    .filter { $0.uid != currentUser.uid }
+            
+            // 4 Create a new DispatchGroup so that we can be notified when all asynchronous tasks are finished executing. We'll use the notify(queue:) method on DispatchGroup as a completion handler for when all follow data has been read
+            let dispatchGroup = DispatchGroup()
+            users.forEach { (user) in
+                dispatchGroup.enter()
+                
+                // 5 Make a request for each individual user to determine if the user is being followed by the current user
+                FollowService.isUserFollowed(user) { (isFollowed) in
+                    user.isFollowed = isFollowed
+                    dispatchGroup.leave()
+                }
+            }
+            
+            // 6 Run the completion block after all follow relationship data has returned
+            dispatchGroup.notify(queue: .main, execute: {
+                completion(users)
+            })
+        })
+    }
+    
+    //fetch the UIDs of all of a given user's followers and return them as an String array
+    static func followers(for user: User, completion: @escaping ([String]) -> Void) {
+        let followersRef = Database.database().reference().child("followers").child(user.uid)
+        
+        followersRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let followersDict = snapshot.value as? [String : Bool] else {
+                return completion([])
+            }
+            
+            let followersKeys = Array(followersDict.keys)
+            completion(followersKeys)
+        })
+    }
     
     
 }
